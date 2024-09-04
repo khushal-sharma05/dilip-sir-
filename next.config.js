97ae4f106d3e70653392415b6ba3fdbf3fa269db
khushal-sharma05 <khushal0122@gmail.com>
2024-09-04 08:27:16 +0530

    Resolved merge conflict in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
-  
-=======
-const nextConfig = { 
->>>>>>> 8ed80a4c71fd51a813f6e81dedb528cd6de3deef
-  reactStrictMode: true,
   images: {
     domains: ['api.thecodecafelab.com'], 
   },
@@ -13,27 +7,27 @@ const nextConfig = {
 
 module.exports = nextConfig;
 // config/formConfig.js
-  [
-  {
-    name: 'name',
-    type: 'text',
-    placeholder: 'Your Name',
-    validation: { required: 'Name is required' },
-  },
-  {
-    name: 'email',
-    type: 'email',
-    placeholder: 'Your Email',
-    validation: { 
-      required: 'Email is required',
-      pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Invalid email address' }
-    },
-  },
-  {
-    name: 'message',
-    type: 'textarea',
-    placeholder: 'Your Message',
-    validation: { required: 'Message is required' },
-  },
-  // Add more fields as needed
-];
+//   [
+//   {
+//     name: 'name',
+//     type: 'text',
+//     placeholder: 'Your Name',
+//     validation: { required: 'Name is required' },
+//   },
+//   {
+//     name: 'email',
+//     type: 'email',
+//     placeholder: 'Your Email',
+//     validation: { 
+//       required: 'Email is required',
+//       pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Invalid email address' }
+//     },
+//   },
+//   {
+//     name: 'message',
+//     type: 'textarea',
+//     placeholder: 'Your Message',
+//     validation: { required: 'Message is required' },
+//   },
+//   // Add more fields as needed
+// ];
